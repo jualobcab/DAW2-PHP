b51526e27a171d8f4ac75289cb0bf34452ad35ec
jualobcab <juanjo.lc2323@gmail.com>
2024-11-17 20:27:49 +0100

    Commit final

--- ProyectoAhorcado/functions.php
+++ ProyectoAhorcado/functions.php
@@ -1,6 +1,4 @@
 <?php
-// Todas las funciones del proyecto
-
 // Palabras posibles
 $palabras = [
     "algoritmo",
@@ -29,25 +27,27 @@ $palabras = [
 ///////////////////////////////////////////////////
 /////////////    FUNCIONES   //////////////////////
 ///////////////////////////////////////////////////
-function palabraAleatoria($array){
+
+function palabraAleatoria($array){  // Devuelve un objeto aleatorio de un array
     $num = rand(0,count($array)-1);
     return $array[$num];
 }
-function reemplazar_palabra_guiones($palabra){
+
+function reemplazar_palabra_guiones($palabra){ // Esta funcion reemplaza las letras por _ si no han sido elegidas
     $lenght = strlen($palabra);
     $res = "";
 
-    for ($i=0; $i < $lenght; $i++) { 
+    for ($i=0; $i < $lenght; $i++) {  // Recorre letra a letra
         if (isset($_SESSION["letras"])){
-            if (letraEnLetrasRegistradas(substr($palabra, $i, 1))){
-                if ($i==$lenght-1){
+            if (letraEnLetrasRegistradas(substr($palabra, $i, 1))){ // Si la letra esta en las elegidas entra y la escribe en mayusculas
+                if ($i==$lenght-1){ 
                     $res.=strtoupper(substr($palabra, $i, 1));
                 }
                 else {
                     $res.=strtoupper(substr($palabra, $i, 1))." ";
                 }
             }
-            else {
+            else { // En caso de que la letra no haya sido acertada pone un _
                 if ($i==$lenght-1){
                     $res.="_";
                 }
@@ -68,7 +68,8 @@ function reemplazar_palabra_guiones($palabra){
 
     return $res;
 }
-function letraEnLetrasRegistradas($letra){
+
+function letraEnLetrasRegistradas($letra){ // Comprueba si una letra está presente en las letras elegidas previamente
     foreach ($_SESSION["letras"] as $l){
         if ($l == $letra){
             return true;
@@ -76,14 +77,14 @@ function letraEnLetrasRegistradas($letra){
     }
     return false;
 }
-function selectorDeImagen($numErrores){
+function selectorDeImagen($numErrores){ // Devuelve la direccion de la imagen correspondiente a la imagen del ahorcado
     return "images/turno".$numErrores.".png";
 }
 
-function inicializarJuego(){
+function inicializarJuego(){  // Inicializa los valores de la sesion
     global $palabras;
     if (!isset($_SESSION["palabra"])){
-        $_SESSION["palabra"]=palabraAleatoria($palabras);
+        $_SESSION["palabra"]=palabraAleatoria($palabras); // Aqui elige la palabra
     }
     if (!isset($_SESSION["turnos"])){
         $_SESSION["turnos"]=0;
@@ -96,8 +97,8 @@ function inicializarJuego(){
     }
 }
 
-function palabraCompleta(){
-    $res = true;
+function palabraCompleta(){  // Esta funcion comprueba que la palabra haya sido acertada
+    $res = true;  
     $listaLetras = str_split($_SESSION["palabra"]);
 
     foreach($listaLetras as $l){
--- ProyectoAhorcado/index.php
+++ ProyectoAhorcado/index.php
@@ -1,9 +1,10 @@
 <?php
-    include_once 'functions.php';
-    session_start();
+    include_once 'functions.php'; // Importar funciones.php
+    session_start(); // Inicias la sesion
 
-    inicializarJuego();
+    inicializarJuego(); // Inicializas la sesion
 
+    // Inicializacion de los mensajes de error
     $mensajeInicio = "¡Comienza el juego! Descubre la palabra antes de que sea demasiado tarde.";
     $mensajeErrorInput = "Por favor introduce una única letra válida";
     $letraRepetida = "La letra ya fue elegida, por favor elige otra letra";
@@ -11,36 +12,36 @@
     $mensajeGanar = "ENHORABUENA !!!! HAS GANADO !!!";
     $mensajeFinalizado = "REINICIA EL JUEGO.";
 
-    $mensajes = [];
+    $mensajes = []; // Array en el que se almacenaran los mensajes
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $_POST['letra'] = strtolower($_POST['letra']);
-        if ($_SESSION['fallos']>=6 || palabraCompleta()){
+        $_POST['letra'] = strtolower($_POST['letra']); // Pasa la letra a minusculas para que no haga diferenciacion de una a otra
+        if ($_SESSION['fallos']>=6 || palabraCompleta()){  // Comprueba si el juego está acabado, es por si recargan la pagina una vez acabado el juego
             array_push($mensajes, $mensajeFinalizado);
         }
-        else if (isset($_POST['letra']) && !empty($_POST['letra']) && ctype_alpha($_POST['letra'])){
-            if (strlen($_POST['letra'])!=1){
+        else if (isset($_POST['letra']) && !empty($_POST['letra']) && ctype_alpha($_POST['letra'])){ // Si se ha introducido algo y ese algo es una letra sigue, en caso contrario da el mensaje de error
+            if (strlen($_POST['letra'])!=1){  // Si el usuario consigue meter mas de 1 caracter manda un mensaje de error
                 array_push($mensajes, $mensajeErrorInput);
             }
-            else {
-                if (!letraEnLetrasRegistradas($_POST['letra'])){
-                    if (str_contains($_SESSION["palabra"],$_POST['letra'])){
+            else { // Si solo es un caracter la entrada hace el siguiente codigo
+                if (!letraEnLetrasRegistradas($_POST['letra'])){  // Si la letra no esta en las letras previamente elegidas ejecuta el resto si no manda un mensaje de que esta repetido
+                    if (str_contains($_SESSION["palabra"],$_POST['letra'])){ // Si la letra está en la palabra comprueba el acierto en caso contrario el error
                         array_push($_SESSION["letras"], strtolower($_POST['letra']));
                         $_SESSION["turnos"]+=1;
                         $mensajeAcierto = "HAS ACERTADO !. La letra ".$_POST['letra']." está includída.";
 
-                        array_push($mensajes, $mensajeAcierto);
+                        array_push($mensajes, $mensajeAcierto); // Si ha acertado todas las letras de la palabra manda el mensaje de acierto y el de victoria
                         if (palabraCompleta()){
                             array_push($mensajes, $mensajeGanar);
                         }
                     }
-                    else {
+                    else { // Aqui entra si la letra no está en la palabra
                         array_push($_SESSION["letras"], strtolower($_POST['letra']));
                         $_SESSION["fallos"]+=1;
                         $_SESSION["turnos"]+=1;
                         $mensajeFallo = "HAS FALLADO !. La letra ".$_POST['letra']." no está en la palabra.";
                         
-                        array_push($mensajes, $mensajeFallo);
+                        array_push($mensajes, $mensajeFallo); // Si es el sexto fallo manda el error y notifica que el jugador ha perdido
                         if ($_SESSION["fallos"]>=6){
                             array_push($mensajes, $mensajePerder);
                         }
@@ -74,17 +75,17 @@
 <body>
     <main>
         <aside>
-                <table> <!-- Tabla donde vienen el turno en el que se está y las letras que se han dicho -->
+                <table> <!-- Tabla donde vienen el turno en el que se está y las letras que se han elegido -->
                     <tr>
                         <td>Turno</td>
                         <td><?php echo $_SESSION["turnos"] ?></td>
                     </tr>
                     <tr>
                         <td>Letras usadas</td>
-                        <td><?php echo implode(" ",$_SESSION["letras"]) ?></td>
+                        <td><?php echo implode(" ",$_SESSION["letras"]) ?></td> <!-- Pone todas las letras que has ido escogiendo -->
                     </tr>
                 </table>
-                <form action method="post" style= <?php if($_SESSION["fallos"]==6 || palabraCompleta()) echo 'display:none;' ?>> <!-- Debe desaparecer si se ha perdido -->
+                <form action method="post" style= <?php if($_SESSION["fallos"]==6 || palabraCompleta()) echo 'display:none;' ?>> <!-- La tabla desaparece si el juego se ha acabado -->
                     <label for="letra">Introduce una letra:</label>
                     <input type="text" name="letra" id="letra" maxlength="1" required>
                     <button type="submit" name="enviar">Enviar</button>
@@ -94,7 +95,7 @@
         <img src=<?php echo selectorDeImagen($_SESSION["fallos"])?> alt=""> <!-- Imagen cambiante -->
         <h2 class="letra"><?php echo reemplazar_palabra_guiones($_SESSION["palabra"])?></h2> <!-- Aqui va los huecos de la palabra -->
         <p class="notice"><?php echo implode("\n",$mensajes) ?></p> <!-- Aquí van los mensajes del juego -->
-        <form method="post" action="fin.php">
+        <form method="post" action="fin.php"> <!-- Botón para reiniciar la partida -->
             <button type="submit">Reiniciar Juego</button>
         </form>
     </main>
